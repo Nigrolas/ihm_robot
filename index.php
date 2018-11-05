a7bef04d6e9187e401e7d7e3ede71c39b6b94278
Nigrolas <mouette@lacroixrouge-brest.fr>
2018-11-05 16:03:22 +0100

    creation de l'ihm de base : 4 boutons et 2 input pour 2 moteur

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon 120eme sites Web</title>
+</head>
+<body style="text-align: center;">
+    <dir id="titre" style="border-style: solid;"><h1>Mon site Web</h1></dir>
+    <div style="border-style: solid;">
+        <p>Salut les amis</p>
+          <a href="https://www.google.fr">Lien vers google</a></br>
+    </div>
+    <div id="php">
+        <?php
+            echo  "Nous sommes aujourd'hui " . date("l").".";
+	    phpinfo();
+        ?>
+    </div>
+    
+</html>
